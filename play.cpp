cad18050adacd38eed3a525ade8a206cd90b6ad3
dmunozso <dmunozso@uncc.edu>
2016-04-24 19:19:12 -0400

    Added files via upload

---
+++ play.cpp
@@ -0,0 +1,22 @@
+#include <cstdlib>
+#include <ctime>
+#include <string>
+#include <iostream>
+using namespace std;
+int rollfunc(string in_str);
+int main(){
+cout << rollfunc("roll");
+
+return 0; }
+int rollfunc(string in_str){	
+int num;
+	
+	while (in_str != "roll"){
+		cout<< "Please type 'roll' to roll the die" << endl;
+	cin >> in_str;}
+
+	if (in_str== "roll"){
+		srand(time(NULL));
+		num= rand() % 6 + 1;}
+//cout << num << endl;
+return num;}
