de809bf074ab72d70df4dcaea4882d5a86c44507
bchau1 <barrychau93@gmail.com>
2016-04-30 12:34:48 -0400

    Added files via upload

--- event.cpp
+++ event.cpp
@@ -7,18 +7,21 @@ void Database()
 	char *token2;
 	string arr[200];
 	char temp[100];
-	char eventTemp[7];
+	char eventTemp[10];
 	int count = 0;
 	string eventLocation[200];
 	int eventLocationX[200];
 	int eventLocationY[200];
 	string eventDescription[200];
 	string response[200];
+	int responseNum[200];
 	string energy[200];
+	int energyNum[200];
 	string move[200];
 	int moveX[200];
 	int moveY[200];
 	string Question[200];
+	int questionNum[200];
 	int j = 0;
 	
 	ifstream database("events.txt");
@@ -61,32 +64,29 @@ void Database()
 			Question[j]= array[5];
 		
 	 }
-	 for(int j = 10; j<17; j++) //Location , Deliminator
+	 for(int j = 1; j<200; j++) //Location , Deliminator
 	 {
-	 	for(int i = 0; i<eventLocation[j].length(); i++)
-		{
-		eventTemp[i] = eventLocation[j].at(i);
-		}
-		 string store[3];
-		 token2 = strtok(eventTemp,",");
-		 int i=0;
-		 while(token2 != NULL)
-		 {
-		 	store[i]= token2;
-			i++;
-			//printw("%s\n",token2);
-			token2 = strtok(NULL,",");
-		 }
-		 store[0] = store[0].substr(0,3);
-		 eventLocationX[j]=atoi(store[0].c_str());
-		 store[1] = store[1].substr(0,3);
-		 eventLocationY[j]=atoi(store[1].c_str());
-	}
+		 int pos = eventLocation[j].find_first_of(',');
+		 eventLocationY[j] = stoi(eventLocation[j].substr(pos+1));
+		 eventLocationX[j] = stoi(eventLocation[j].substr(0,pos));
+		 responseNum[j] = stoi(response[j]);
+		 energyNum[j] = stoi(energy[j]);
+		 questionNum[j] = stoi(Question[j]);
+		 
+	 }
+	 for(int j = 1; j<200; j++) //Location , Deliminator
+	 {
+		 int pos = move[j].find_first_of(',');
+		 moveY[j] = stoi(move[j].substr(pos+1));
+		 moveX[j] = stoi(move[j].substr(0,pos));
+	 }
+	 
+	 /*
 	ofstream test ("testing.txt");
 	
 		for(int i =1; i<200; i++)
 		{
-		test<<eventLocation[i]<<endl;
+		//test<<eventLocation[i]<<endl;
 		//test<<eventDescription[i]<<endl;
 		//test<<response[i]<<endl;
 		//test<<energy[i]<<endl;
@@ -94,6 +94,8 @@ void Database()
 		//test<<Question[i]<<endl;
 		//test<<eventLocationX[i]<<"   ";
 		//test<<eventLocationY[i]<<endl;
+		//test<<moveX[i]<<"   ";
+		//test<<moveY[i]<<endl;
 		}
-		test.close();
+		test.close();*/
 }
