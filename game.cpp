7fd71580c8adcebb9acb3246db18acae6a91b288
bchau1 <barrychau93@gmail.com>
2016-05-02 19:19:01 -0400

    Added files via upload

--- game.cpp
+++ game.cpp
@@ -1,24 +1,36 @@
 #include"dork.h"
-
+//cloud 9
 class dorkInfo{
 	private:
 		string name= "Unknown";
 		string level = "Terrestrial";
-		int energy= 0;
 		int rolls = 0;
+		int energy= 15;
 		int steps = 0;
+		int age = 0;
+		string nationality = "Unknown";
+		int currentLocationX = 0;
+		int currentLocationY = 0;
 	public:
 		void setName(string set_name);
 		void setLevel(string set_level);
 		void setEnergy(int set_energy);
 		void setRolls(int set_rolls);
 		void setSteps(int set_steps);
+		void setAge(int set_age);
+		void setNationality(string set_nationality);
+		void setCurrentLocationX(int set_LocX);
+		void setCurrentLocationY(int set_LocY);
 		void printAll();
 		string getName();
 		string getLevel();
 		int getEnergy();
 		int getRolls();
 		int getSteps();
+		int getAge();
+		string getNationality();
+		int getCurrentLocationX();
+		int getCurrentLocationY();
 };
 void dorkInfo::setName(string set_name)
 {
@@ -40,6 +52,22 @@ void dorkInfo::setSteps(int set_steps)
 {
 	steps = set_steps;
 }
+void dorkInfo::setAge(int set_age)
+{
+	age = set_age;
+}
+void dorkInfo::setNationality(string set_nationality)
+{	
+	nationality = set_nationality;
+}
+void dorkInfo::setCurrentLocationX(int set_LocX)
+{
+	currentLocationX = set_LocX;
+}
+void dorkInfo::setCurrentLocationY(int set_LocY)
+{
+	currentLocationY = set_LocY;
+}
 string dorkInfo::getName()
 {
 	return name;
@@ -60,12 +88,37 @@ int dorkInfo::getSteps()
 {
 	return steps;
 }
+int dorkInfo::getAge()
+{
+	return age;
+}
+string dorkInfo::getNationality()
+{
+	return nationality;
+}
+int dorkInfo::getCurrentLocationX()
+{
+	return currentLocationX;
+}
+int dorkInfo::getCurrentLocationY()
+{
+	return currentLocationY;
+}
 void dorkInfo::printAll()
 {
-	//addstr(name);
-	cout<<"Name: " <<name<<"    "<< "Level: "<<level <<"    "<< "Rolls left: " <<rolls <<"    "<< "Engery Left: "<<energy <<"    "<< "Steps: "<<steps << endl;
-	/*cout<<level;
-	cout<<rolls;
-	cout<<energy;
-	cout<<steps;*/
+	printw("Name: ");
+	printw("%s\t", name.c_str());
+	printw("Level: ");
+	printw("%s\t", level.c_str());
+	printw("Rolls: ");
+ 	printw("%d\t", rolls);
+	printw("Energy: ");
+	printw("%d\t", energy);
+	printw("\n\t\t\tSteps: ");
+ 	printw("%d\t", steps);
+	printw("Location: ");
+	printw("%d", currentLocationX);
+	printw(",");
+ 	printw("%d", currentLocationY);
+	printw("\n");
 }
